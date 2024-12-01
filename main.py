0a8346ff9c15cb9082c41d1b130396ed5c123293
Premalatha Balan <premalatha_balan@yahoo.co.uk>
2024-12-01 15:51:46 +0000

    organised the files

--- main.py
+++ main.py
@@ -3,30 +3,14 @@ f = open("day1p1.txt", "r")
 idlst1, idlst2 = [], []
 lines_lst_str = f.readlines()
 for line in lines_lst_str:
-  #print(line)
   line_lst = line.split()
-  #print(line_lst)
   idlst1.append(int(line_lst[0]))
   idlst2.append(int(line_lst[1]))
-  #print(f"idlst1: {idlst1}")
-  #print(f"and idlst2: {idlst2}")
-  #y = input("enter a key :")
 
-#print(f"idlst1: {idlst1}")
-#print(f"and idlst2: {idlst2}")
+ct = lambda same: idlst2.count(same)
 
-idlst1.sort()
-idlst2.sort()
-dist = 0
-
-diff = lambda x, y: abs(x - y)
-
+score=0
 for i in range(len(idlst1)):
-  dist+=(diff(idlst1[i], idlst2[i]))
-  """
-  
-  print(f"idlst1[{i}]: {idlst1[i]} and idlst2[{i}]: {idlst2[i]}")
-  print(f"dist: {dist}")
-  y = input("enter a key :") """
-
-print(f"dist: {dist}")
+  if i in idlst2: 
+    score += ct(i)
+print(score)
