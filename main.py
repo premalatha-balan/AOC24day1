388de5a3eb78ce5d4f977e73773e8fb5c41c285a
Premalatha Balan <premalatha_balan@yahoo.co.uk>
2024-12-17 15:50:30 +0000

    splitting works. need to check the edge cases

--- main.py
+++ main.py
@@ -1,26 +1,56 @@
 import re
 
+def split_n_del(line):
+  new_line=re.split(r'(do\(\))', line)
+  new_line.pop(0)
+  return new_line [1::2]
+
+
+
 f = open("day3_input.txt", "r")
 
 memory = [line.strip() for line in f]
 
 tot = 0
 dontsplit = re.split(r'(don\'t\(\))', memory[0]) 
-#print(dontsplit[0])
-#print(dontsplit[1])
-#print(dontsplit[2])
-#print(dontsplit[3])
+
 mulmatch=re.findall(r'(mul\((\d+\d*\d*),(\d+\d*\d*\)))',dontsplit[0])
-#print(mulmatch)
+
 mults = [int(j[1])*int(j[2].strip(")")) for j in mulmatch]
 tot+=sum(mults)
 print(f"tot is {tot}")
 
+allnotdonts = dontsplit[2::2]
 
-for i in range(len(memory)):
-  print(memory[i] if i<10 else 'stop program')
-  mulmatch=re.findall(r'(mul\((\d+\d*\d*),(\d+\d*\d*\)))',memory[i])
+dotext = split_n_del(allnotdonts[-1])
 
+dotextall=[]
+for i in range(len(allnotdonts)):
+  dotext = split_n_del(allnotdonts[i])
+  if len(dotext)!=0: dotextall.extend(dotext)
+
+#print(f"length of useful text is {len(dotextall)}")
+#y = input("enter a key:")
+for j in range(1,len(memory)):
+  dontsplit = re.split(r'(don\'t\(\))', memory[j])
+  if dontsplit[0]!="don\'t()":
+    allnotdonts = dontsplit[::2]
+    #print("first is not don't")
+  else:
+    allnotdonts = dontsplit[1::2]
+    #print("first is don't")
+  #print(len(allnotdonts))
+  #y = input("enter a key:")
+
+  for i in allnotdonts:
+    dotext = split_n_del(i)
+    if len(dotext)!=0: dotextall.extend(dotext)
+    #print(f"length of useful text is {len(dotextall)}")
+    #y = input("enter a key:")
+
+for i in dotextall:
+  mulmatch=re.findall(r'(mul\((\d+\d*\d*),(\d+\d*\d*\)))',i)
+  
   mults = [int(j[1])*int(j[2].strip(")")) for j in mulmatch]
   tot+=sum(mults)
   #print(mults)
