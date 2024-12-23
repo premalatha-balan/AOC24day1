22a0d7f3da8086ccb7278c81c13dd10acfeb4858
Premalatha Balan <premalatha_balan@yahoo.co.uk>
2024-12-23 16:53:46 +0000

    need to work on optimising the way it works to avoid edge cases

--- d23p1/d23p1.py
+++ d23p1/d23p1.py
@@ -4,14 +4,31 @@ f = open("/workspaces/Advent-of-Code-2024/d23p1/day23_input.txt", "r")
 line = f.readlines()
 nets = [i.strip().split("-") for i in line]
 
-for i in nets:
-    if (i[0][0] or i[1][0]) == "t":
-        print(i)
-        y=input("enter a key")
-        for j in nets:
-            if (i[0] or i[1]) in j and (i!=j):
-                print(j)
-                y=input("enter a key")
-                if j[0] not in i:
-                    
+count = 0
+# for i in nets:
+#     if (i[0][0] or i[1][0]) == "t":
+#         print(i)
+#         y=input("enter a key")
+#         for j in nets:
+#             if (i[0] or i[1]) in j and (i!=j):
+#                 print(j)
+#                 y=input("enter a key")
+#                 if (j[0] not in i) and (i[1] not in j):
+#                     if ([j[0],i[1]] in nets): count+=1
+#                 elif (j[0] not in i) and (i[0] not in j):
+#                     if ([j[0],i[0]] in nets): count+=1
+#                 elif (j[1] not in i) and (i[1] not in j):
+#                     if ([j[1],i[1]] in nets): count+=1
+#                 elif (j[1] not in i) and (i[0] not in j):
+#                     if ([j[1],i[0]] in nets): count+=1
+#                 print(f"count = {count} for {i} and {j}")
+#                 y = input("enter a key")
+
+
+
+    
+
+
+
+
 
