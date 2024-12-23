7596cddbbe5931eb8f0e9e448d3146c37f338ef1
Premalatha Balan <premalatha_balan@yahoo.co.uk>
2024-12-23 17:35:12 +0000

    not easy

--- d23p1/d23p1.py
+++ d23p1/d23p1.py
@@ -1,18 +1,21 @@
 
+
 f = open("/workspaces/Advent-of-Code-2024/d23p1/day23_input.txt", "r")
 
 line = f.readlines()
 nets = [i.strip().split("-") for i in line]
 
+
+
 count = 0
 # for i in nets:
 #     if (i[0][0] or i[1][0]) == "t":
-#         print(i)
-#         y=input("enter a key")
+#         #print(i)
+#         #y=input("enter a key")
 #         for j in nets:
 #             if (i[0] or i[1]) in j and (i!=j):
-#                 print(j)
-#                 y=input("enter a key")
+#                 #print(j)
+#                 #y=input("enter a key")
 #                 if (j[0] not in i) and (i[1] not in j):
 #                     if ([j[0],i[1]] in nets): count+=1
 #                 elif (j[0] not in i) and (i[0] not in j):
@@ -21,8 +24,23 @@ count = 0
 #                     if ([j[1],i[1]] in nets): count+=1
 #                 elif (j[1] not in i) and (i[0] not in j):
 #                     if ([j[1],i[0]] in nets): count+=1
-#                 print(f"count = {count} for {i} and {j}")
-#                 y = input("enter a key")
+#                 #print(f"count = {count} for {i} and {j}")
+#                 #y = input("enter a key")
+
+print(f"count = {count}")
+
+# def triangle(t_list, big_list):
+#     for i in big_list:
+#         if i!=t_list[0]:
+#             if i[0] == t_list[0] and [i[1],t_list[1]] in big_list: count+=1
+
+for i in nets:
+    if (i[0][0] or i[1][0]) == "t":
+        for j in nets:
+            if i!=j:
+                
+
+
 
 
 
