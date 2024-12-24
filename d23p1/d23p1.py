ef23d11db758dbec6c1c2861656ffa83bf1b3c43
Premalatha Balan <premalatha_balan@yahoo.co.uk>
2024-12-24 09:47:02 +0000

    sing match now

--- d23p1/d23p1.py
+++ d23p1/d23p1.py
@@ -34,17 +34,71 @@ count = 0
 #         if i!=t_list[0]:
 #             if i[0] == t_list[0] and [i[1],t_list[1]] in big_list: count+=1
 
+# for i in nets:
+#     if (i[0][0] or i[1][0]) == "t":
+#         for j in nets:
+#             if i!=j and ((j[0] in i) or (j[1] in i)):
+#                 #print(f"i={i} and j={j}")
+#                 #y=input("enter a key")
+
+#                 ti = 
+
+#                 # if i[0][0]=="t": 
+#                 #     apex1=i[1]
+
+#                 # elif i[1][0]=="t": apex1=i[0]
+#                 # else: apex1 =""
+#                 # if j[0][0]=="t": apex2=j[1]
+#                 # elif j[1][0]=="t": apex2=j[0]
+#                 # else: apex2=""
+#                 # if ([apex1, apex2] in nets) or ([apex2, apex1] in nets): count+=1
+
+
+apex1, apex2 = "", ""
+
+# for i in nets:
+#     if (i[0][0] or i[1][0]) == "t":
+#         for j in nets.remove(i):
+#             if j[0] or j[1] in i:
+#                 if j[0] == i[0]:
+#                     apex1=j[1]
+#                     apex2=i[1]
+#                 elif j[0] == i[1]:
+#                     apex1=j[1]
+#                     apex2=i[0]
+#                 elif j[1]==i[0]:
+#                     apex1=j[0]
+#                     apex2=i[1]
+#                 elif j[1]==i[1]:
+#                     apex1 = j[0]
+#                     apex2 = i[2]
+
+
 for i in nets:
     if (i[0][0] or i[1][0]) == "t":
-        for j in nets:
-            if i!=j and (j[0] or j[1] in i):
-                if i[0][0]=="t": apex1=i[1][0]
-                elif i[1][0]=="t": apex1=i[0][0]
-                else: apex1 =""
-                if j[0][0]=="t": apex2=j[1][0]
-                elif j[1][0]=="t": apex2=j[0][0]
-                else: apex2=""
-                if ([apex1, apex2] in nets) or ([apex2, apex1] in nets): count+=1
+        for j in nets.remove(i):
+            if j[0] or j[1] in i:
+                match(j[0]):
+                    case i[0]:
+                        apex1=j[1]
+                        apex2=i[1]
+                    case i[1]:
+                        apex1=j[1]
+                        apex2=i[0]
+                match (j[1]):
+                    case i[0]:
+                        apex1=j[0]
+                        apex2=i[1]
+                    case i[1]:
+                        apex1=j[0]
+                        apex2=i[0]
+
+                
+
+
+                
+
+
 
 print(f"count = {count}")
 
