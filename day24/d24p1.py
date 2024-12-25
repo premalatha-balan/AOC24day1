b99edaefcf53f9142ebce943c882c2473d36a9d5
Premalatha Balan <premalatha_balan@yahoo.co.uk>
2024-12-25 12:20:27 +0000

    the answer is not correct for the test data

--- day24/d24p1.py
+++ day24/d24p1.py
@@ -1,11 +1,11 @@
 
-f = open("/workspaces/Advent-of-Code-2024/day24/day24_input.txt", "r")
+f = open("/workspaces/Advent-of-Code-2024/day24/testdata.txt", "r")
 line = f.readlines()
 #starts = [line[i].strip().split(": ") for i in range(90)]
 #print(starts)
 connects = [l.strip().split() for l in line]
 #print(connects[91])
-starts = connects[:90]
+starts = connects[:10]
 #print(starts[0])
 
 starts_dict = {s[0].strip(":"):int(s[1]) for s in starts}
@@ -13,8 +13,9 @@ starts_dict = {s[0].strip(":"):int(s[1]) for s in starts}
 #print(starts_dict)
 #print(starts[89])
 #print(len(starts))
-connects = connects[91:]
-#print(connects[0])
+connects = connects[11:]
+# print(connects[0])
+# y=input("enter a key")
 
 
 # for conx in connects:
@@ -39,28 +40,28 @@ while True:
                 # y=input("enter a key")
                 match conx[1]:
                     case "AND":
-                        print(conx[0], starts_dict[conx[0]])
-                        print(conx[2], starts_dict[conx[2]])
+                        # print(conx[0], starts_dict[conx[0]])
+                        # print(conx[2], starts_dict[conx[2]])
                         # y=input("enter a key")
                         output = starts_dict[conx[0]]*starts_dict[conx[2]]
-                        print(f"{conx[4]} is {output} for {conx[1]}")
-                        y=input("enter a key")
+                        # print(f"{conx[4]} is {output} for {conx[1]}")
+                        # y=input("enter a key")
                         starts_dict[conx[4]] = output
                     case "OR":
-                        print(conx[0], starts_dict[conx[0]])
-                        print(conx[2], starts_dict[conx[2]])
+                        # print(conx[0], starts_dict[conx[0]])
+                        # print(conx[2], starts_dict[conx[2]])
                         if starts_dict[conx[0]]+starts_dict[conx[2]] !=0: output = 1
                         else: output = 0
-                        print(f"{conx[4]} is {output} for {conx[1]}")
-                        y=input("enter a key")
+                        # print(f"{conx[4]} is {output} for {conx[1]}")
+                        # y=input("enter a key")
                         starts_dict[conx[4]] = output
                     case "XOR":
-                        print(conx[0], starts_dict[conx[0]])
-                        print(conx[2], starts_dict[conx[2]])
+                        # print(conx[0], starts_dict[conx[0]])
+                        # print(conx[2], starts_dict[conx[2]])
                         if starts_dict[conx[0]]!=starts_dict[conx[2]]: output = 1 
                         else: output = 0
-                        print(f"{conx[4]} is {output} for {conx[1]}")
-                        y=input("enter a key")
+                        # print(f"{conx[4]} is {output} for {conx[1]}")
+                        # y=input("enter a key")
                         starts_dict[conx[4]] = output
                 # print(f"key is {conx[4]} and value is {output}")
                 # print(f"key is {conx[4]} and value is {starts_dict[conx[4]]}")
@@ -85,7 +86,7 @@ zs={s:starts_dict[s] for s in starts_dict if s[0]=="z"}
 # print(len(zs))    
 zs_keys = list(zs.keys())
 zs_keys.sort(reverse=True)
-print(zs_keys)
+# print(zs_keys)
 
 zbin_lst = [str(zs[sk]) for sk in zs_keys]
 #print(zbin_lst)
